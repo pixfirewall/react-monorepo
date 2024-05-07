493c7b37ef1d5ca82a676c75b54eb1b6297a0db2
Amir Hodaee <amir.hodaee@lftt.se>
2024-05-07 02:45:24 +0200

    custom component example

---
+++ packages/ui/src/Image/CustomImage.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+// https://mui.com/material-ui/integrations/interoperability/#styled-components
+// https://emotion.sh/docs/introduction
+export const CustomImage = styled.img``
